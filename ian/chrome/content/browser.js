f68330bdc83f838b6af76dce714381687901ed16
Ian <igard@umich.edu>
2011-10-27 22:05:07 -0400

    kind of got jquery working in firefox extension

--- ian/chrome/content/browser.js
+++ ian/chrome/content/browser.js
@@ -1,6 +1,9 @@
 let DisplayText = {
 	onCommand: function(event) {
-	  		
+		dump("iangardiner.loadjQuery()\n");
+		var iangardiner;
+		jquery = loadjQuery(iangardiner);
+	 
 		// Append text to end of web page
 /*		var headertext = content.document.createTextNode("Random inserted information asdfas dfsadjfk")
 		content.document.body.appendChild(headertext)
@@ -11,15 +14,132 @@ let DisplayText = {
 		var headertext2 = content.document.createTextNode("Popup window information")
 		my_window.content.document.body.appendChild(headertext2)
 
+		dump("Text displayed\n");
+
 		// Add text after first 'h1' element of page
 		var doc = content.document;
 		var theNewParagraph = doc.createElement('p');
 		var theTextOfTheParagraph = doc.createTextNode('Some content.');
 		theNewParagraph.appendChild(theTextOfTheParagraph);
 		var header = doc.getElementsByTagName("h1");
-		header[0].appendChild(theNewParagraph);
-		
+		//header[0].appendChild(theNewParagraph);
 
-		dump("Text displayed\n");
-		}
+		dump("about to call wikipedia\n");
+		
+		callWikipediaAPI("Philosophy");
+
+	}
 };
+
+
+var wikipediaHTMLResult = function(data) {
+	dump("wikipediaHTMLResult\n");
+/*	document.write("<p>Inside callback</p>");
+	document.write("Text data: " + data);
+	var readData;
+	var i = 0;
+	for(x in data){
+		for(y in data[x]){
+			for(z in data[x][y]){
+				readData += data[x][y][z];
+			}
+		}
+	}
+	var arrayData = jQuery.makeArray(readData);
+	var newData = new Array();
+	var ignore = false;
+	var i = 0;
+	for(x in readData){
+		if(i > 100000){
+			break;
+		}
+		i++;
+		if(readData[x]=== '<'){
+			ignore = true;
+		}
+		else if(readData[x] === '>'){
+			ignore = false;
+		}
+		else if(readData[x]==='"'){
+			if(ignore){
+				newData[x] = "";
+			}
+			else{
+				newData[x] = "&quot;";
+			}
+		}
+		else{
+			if(ignore){
+				newData[x] = "";
+			}
+			else{
+				newData[x] = readData[x];
+			}
+		}
+	}
+	i = 0;
+	for(x in newData){
+		document.write(newData[x]);
+		i++;
+	}
+	document.write("<p>"+i+"</p>");
+*/};
+function callWikipediaAPI(wikipediaPage) {
+	dump("var jQuery\n");
+	var jQuery = jquery;
+	var $ = function(selector,context){
+		   return new jQuery.fn.init(selector,context||window._content.document);
+	};
+	dump("$.fn = $.prototype\n");
+	$.fn = $.prototype = jQuery.fn;
+	dump("iangardiner\n");
+	env = window._content.document;
+
+
+	dump("callWikipediaAPI\n");
+	// http://www.mediawiki.org/wiki/API:Parsing_wikitext#parse
+	jqxhr = jquery.getJSON('http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?', {page:wikipediaPage, prop:"text|images", uselang:"en"}, function() {dump("success?\n");} )
+		.error(function() { dump("error?\n"); } );
+	jqxhr.error(function() {dump("error?\n");} );
+	dump(jqxhr + "\n");
+	for(x in jqxhr){
+		dump(x + "\n");
+		dump(jqxhr[x] + "\n");
+	}
+	var info = jquery.get('http://www.wikipedia.org/w/api.php?action=query&prop=revisions$rvprop=content&format=xml&titles=Main%20Page', function(data) {
+			dump('Get1 was performed.\n');
+			for(x in data){
+				dump("\n\ndata from element " + x + ":\n" + data[x] + "\n");
+				for(y in data[x]){
+					dump("data from element " + y + " in " + x + ":\n" + data[x][y] + "\n");
+/*					for(z in data[x][y]){
+						dump(data[x][y][z] + x + y + z + "\n");
+					}
+*/				}
+			}
+		});
+
+/*	for(x in info){
+		dump(x + "\n");
+		//dump(info[x] + "\n");
+		for(y in info[x]){
+			dump(y + "\n");
+		}
+	}
+*/	dump("gotJSON()\n");
+}
+
+function loadjQuery(context){
+	dump("loadjQuery\n");
+	var loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"].getService(Components.interfaces.mozIJSSubScriptLoader);
+	loader.loadSubScript("chrome://ian/content/jquery.js",context);
+	
+	dump("var jQuery =\n");
+	var jQuery = window.jQuery.noConflict(true);
+	dump("exiting1 loadjQuery\n");
+	if( typeof(jQuery.fn._init) == 'undefined') { jQuery.fn._init = jQuery.fn.init; }
+	dump("exiting2 loadjQuery\n");
+	return jQuery;
+}
+
+
