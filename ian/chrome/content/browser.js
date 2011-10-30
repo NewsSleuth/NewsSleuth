82a6149d7962627045a09b5a42c2398b7052cc71
Ian <igard@umich.edu>
2011-10-30 18:58:46 -0400

    got parser working- just needs refinement

--- ian/chrome/content/browser.js
+++ ian/chrome/content/browser.js
@@ -8,11 +8,11 @@ let DisplayText = {
 		content.document.body.appendChild(headertext)
 	 
 		// Create a new window to display text
-		my_window = window.open("", "mywindow1", "status=1,width=350,height=150");
+/*		my_window = window.open("", "mywindow1", "status=1,width=350,height=150");
 		//my_window = window.open("", "mywindow1");
 		var headertext2 = content.document.createTextNode("Popup window information")
 		my_window.content.document.body.appendChild(headertext2)
-
+*/
 		dump("Text displayed\n");
 
 		// Add text after first 'h1' element of page
@@ -25,154 +25,138 @@ let DisplayText = {
 
 		dump("about to call wikipedia\n");
 		
-		callWikipediaAPI("Gregg Hartsuff");
+		callWikipediaAPI("Bill Clinton");
 
 	}
 };
 
 
-var wikipediaHTMLResult = function(data) {
-	dump("wikipediaHTMLResult\n");
-/*	document.write("<p>Inside callback</p>");
-	document.write("Text data: " + data);
-	var readData;
-	var i = 0;
-	for(x in data){
-		for(y in data[x]){
-			for(z in data[x][y]){
-				readData += data[x][y][z];
-			}
-		}
-	}
-	var arrayData = jQuery.makeArray(readData);
-	var newData = new Array();
-	var ignore = false;
-	var i = 0;
-	for(x in readData){
-		if(i > 100000){
-			break;
-		}
-		i++;
-		if(readData[x]=== '<'){
-			ignore = true;
-		}
-		else if(readData[x] === '>'){
-			ignore = false;
-		}
-		else if(readData[x]==='"'){
-			if(ignore){
-				newData[x] = "";
-			}
-			else{
-				newData[x] = "&quot;";
-			}
-		}
-		else{
-			if(ignore){
-				newData[x] = "";
-			}
-			else{
-				newData[x] = readData[x];
-			}
-		}
-	}
-	i = 0;
-	for(x in newData){
-		document.write(newData[x]);
-		i++;
-	}
-	document.write("<p>"+i+"</p>");
-*/};
 function callWikipediaAPI(wikipediaPage) {
-/*	dump("var jQuery\n");
-	var jQuery = jquery;
-	var $ = function(selector,context){
-		   return new jQuery.fn.init(selector,context||window._content.document);
-	};
-	dump("$.fn = $.prototype\n");
-	$.fn = $.prototype = jQuery.fn;
-	dump("iangardiner\n");
-	env = window._content.document;
-*/
 
 	dump("callWikipediaAPI\n");
 	// http://www.mediawiki.org/wiki/API:Parsing_wikitext#parse
-	$.getJSON("http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?", {page:wikipediaPage, prop:"text|images", uselang:"en"}, function() {dump("success?\n");} )
-		.error(function() { dump("error?\n"); } );
-//	jqxhr.error(function() {dump("error?\n");} );
-//	dump(jqxhr + "\n");
-/*	jqxhr = $.get('http://www.wikipedia.org/w/api.php?action=raw&prop=revisions$rvprop=content&format=xml&titles=Philosophy', function(data) {
-			dump("success2?\n");
-			for(x in data){
-				dump(x + "\n");
-				dump(jqxhr[x] + "\n");
-				}
-			dump("title: " + data.title + "\n");
-			}, 'xml');
-*/	$.ajax({
-		url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?",
-		dataType: 'json',
-		data: {page:wikipediaPage, prop:"text|images", uselang:"en"},
-		success: function() {dump("success in ajax");},
-		error: function() {dump("error in ajax");}
-	});
-
 
 	var remoteApi = JsMwApi("http://en.wikipedia.org/w/api.php", "local");
-	remoteApi({action: "query", prop: "revisions", rvprop: "content", titles: "Alphabet"}, function (res){ 
-		dump("inside callback for jsmwapi");
+	remoteApi({action: "query", prop: "revisions", rvprop: "content", titles: wikipediaPage}, function (res){ 
+		dump("inside callback for jsmwapi\n");
 	    for(var page in res.query.pages){
-	        alert(res.query.pages[page].title);
+	        //alert(res.query.pages[page].title);
+			dump("page: " + page);
 			for(x in res.query.pages[page]){
 				dump(x + "\n\t" + res.query.pages[page][x] + "\n");
 			}
 			for(x in res.query.pages[page]["revisions"]){
-				dump(x + "\n\t" + res.query.pages[page]["revisions"][x] + "\n");
+				dump("\t" + x + "\n\t\t" + res.query.pages[page]["revisions"][x] + "\n");
 				for(y in res.query.pages[page]["revisions"][x]){
-					dump(y + "\n\t" + res.query.pages[page]["revisions"][x][y] + "\n");
+					//dump("\t\t" + y + "\n\t\t" + res.query.pages[page]["revisions"][x][y] + "\n");
 				}
 			}
+			var data;
+			var count = 0;
+			for(i in res.query.pages[page]["revisions"][0]){
+				data = res.query.pages[page]["revisions"][0][i];
+				for(j in res.query.pages[page]["revisions"][0][i]){
+					//data[count++] = res.query.pages[page]["revisions"][0][i][j];
+				}
+			}
+			var result = parseFirstThou(data);
+			dump("result:\n" + result + "\n");
+			result = firstP(result);
+			dump("first paragraph: \n" + result + "\n");
 		}
 	});
 
-//	for(x in jqxhr){
-//		dump(x + "\n");
-//		dump(jqxhr[x] + "\n");
-//	}
-/*	var info = jquery.get('http://www.wikipedia.org/w/api.php?action=query&prop=revisions$rvprop=content&format=xml&titles=Main%20Page', function(data) {
-			dump('Get1 was performed.\n');
-			for(x in data){
-				dump("\n\ndata from element " + x + ":\n" + data[x] + "\n");
-				for(y in data[x]){
-					dump("data from element " + y + " in " + x + ":\n" + data[x][y] + "\n");
-					for(z in data[x][y]){
-						dump(data[x][y][z] + x + y + z + "\n");
-					}
-				}
-			}
-		});
-*/
-/*	for(x in info){
-		dump(x + "\n");
-		//dump(info[x] + "\n");
-		for(y in info[x]){
-			dump(y + "\n");
-		}
-	}
-*/	dump("gotJSON()\n");
+	dump("gotJSON()\n");
 }
 
-function loadjQuery(context){
-	dump("loadjQuery\n");
-	var loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"].getService(Components.interfaces.mozIJSSubScriptLoader);
-	loader.loadSubScript("chrome://ian/content/jquery.js",context);
-	
-	dump("var jQuery =\n");
-	var jQuery = window.jQuery.noConflict(true);
-	dump("exiting1 loadjQuery\n");
-	if( typeof(jQuery.fn._init) == 'undefined') { jQuery.fn._init = jQuery.fn.init; }
-	dump("exiting2 loadjQuery\n");
-	return jQuery;
+function parseFirstThou(data){
+
+	var result = new String("");
+	var placeholder = new String("");
+	var count = 0;
+	var braceIgnore = false;
+	var bracketIgnore = false;
+	var tagIgnore = false;
+	var apostropheIgnore = false;
+	var brackets = 0;
+	var braces = 0;
+	var tag = 0;
+	var file = 0;
+	for(var i = 0; count < 3000 && i < data.length; i++){
+		
+		if(data[i] === '\'' && data[i+1] === '\'' && data[i+2] === '\''){
+			apostropheIgnore = true;
+		}
+		if(data[i-1] === '\'' && data[i-2] === '\'' && data[i-3] === '\''){
+			apostropheIgnore = false;
+		}
+		if(data[i-1] === '}' && data[i-2] === '}'){
+			braces--;
+		}
+		if(data[i] === '{' && data[i+1] ==='{'){
+			braces++;
+		}
+		if(data[i-1] === '>' && data[i-2] === 'f' && data[i-3] === 'e' && data[i-4] === 'r' && data[i-5] === '/' && data[i-6] === '<'){
+			tag--;
+		}
+		if(data[i] === '<' && data[i+1] === 'r' && data[i+2] === 'e' && data[i+3] === 'f' && data[i+4] === '>'){
+			tag++;
+		}
+		if(data[i] === '[' && data[i+1] === '['){
+			brackets++;
+		}
+		if(data[i-1] === ']' && data[i-2] === ']'){
+			brackets--;
+		}
+		if(braces === 0){
+			braceIgnore = false;
+		}
+		else braceIgnore = true;
+		if(tag === 0){
+			tagIgnore = false;
+		}
+		else tagIgnore = true;
+		if(brackets === 0){
+			bracketIgnore = false;
+		}
+		else{
+			if(tagIgnore === false && braceIgnore === false && apostropheIgnore === false){
+			}
+			else{
+				bracketIgnore = true;
+			}
+		}
+
+		if(tagIgnore === false && bracketIgnore === false && braceIgnore === false && apostropheIgnore === false){
+			result = String.concat(result, data[i]);
+			count++;
+		}
+	}
+	dump("\ncount: " + count + "\n");
+	return result;
+}
+
+
+function firstP(data){
+	var result = new String("");
+	var beginning = true;
+	for(var i = 0; i < data.length; i++){
+		if(beginning && (data.charAt(i) === '\n' || data.charAt(i) === '\t' || data.charAt(i) === '\ ')){
+			dump("space\n");
+		}
+		else if(beginning === false && data.charAt(i) != '\n'){
+			result = String.concat(result, data.charAt(i));
+		}
+		else if(beginning){
+			beginning = false;
+			result = String.concat(result, data.charAt(i));
+		}
+		else{
+			dump("break\n");
+			break;
+		}
+	}
+	return result;
 }
 
 
